5cf020295ad4da27a02e98de7eab6af9b0dff8c1
Mirtau <tjj72@comcast.net>
2017-10-11 13:35:06 -0600

    DOM codes

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
-function changePic (){
+function changePic() {
   var main = document.getElementById('image');
-    main.src = 'images/fallow-deer-602253__480.jpg';
+  main.src = 'images/fallow-deer-602253__480.jpg';
 }
 
 
 //Change the headings text color to blue
 //HINT: it's a class!
-
+var headings = document.getElementsByClassName('headings');
+for (var i = 0; i < headings.length; i++) {
+  headings[i].style.color = 'blue';
+}
 
 //Change the button's background-color to green
-
+var button = document.getElementById('button');
+button.style.backgroundColor = 'green';
+console.log(button);
 
 //Change the paragraph font to 'Tahona'
+var pChange = document.getElementsByClassName('paragraph')[0];
+pChange.style.fontFamily = 'Wingdings';
 
 
 //Change the entire documents background color to yellow
+
+var brchange = document.getElementsByTagName('Body')[0];
+brchange.style.backgroundColor = 'yellow';
